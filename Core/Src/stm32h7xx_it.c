1819b95c550baeeb4af5903f61ff94391e035752
cjw8809 <46369795+cjw8809@users.noreply.github.com>
2020-08-27 02:21:32 +0900

    edit .ioc inconsistance

--- Core/Src/stm32h7xx_it.c
+++ Core/Src/stm32h7xx_it.c
@@ -334,10 +334,9 @@ void I2C2_ER_IRQHandler(void)
 void USART2_IRQHandler(void)
 {
   /* USER CODE BEGIN USART2_IRQn 0 */
-
-  /* USER CODE END USART2_IRQn 0 */
 	Telem_IdleHandler();
-	HAL_UART_IRQHandler(&huart2);
+  /* USER CODE END USART2_IRQn 0 */
+  HAL_UART_IRQHandler(&huart2);
   /* USER CODE BEGIN USART2_IRQn 1 */
 
   /* USER CODE END USART2_IRQn 1 */
